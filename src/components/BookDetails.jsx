ea8ce1d69f710828bab856d3f34dd97d77e71c7d
Miguel Pacillo <mpaci006@fiu.edu>
2021-02-12 15:12:14 -0500

    Start on book details

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -4,6 +4,8 @@ import React from "react";
 
 const book = {
   author: "J.K. Rowling",
+  authorBio:
+    "Joanne Rowling CH, OBE, HonFRSE, FRCPE, FRSL, better known by her pen name J. K. Rowling, is a British author and philanthropist. She is best known for writing the Harry Potter fantasy series.",
   cover:
     "https://images-na.ssl-images-amazon.com/images/I/51jyI6lYi1L._SX342_BO1,204,203,200_.jpg",
   genre: "Fiction",
@@ -16,33 +18,67 @@ const book = {
 };
 
 function BookDetails() {
-  let space = "1vh";
-
   return (
-    <div class="container-md rounded border" id="detailscontainer">
+    <div class="container" id="detailscontainer">
       <div class="row">
-        <div class="col-md-4">
-          <img
-            class="rounded border"
-            id="bookcover"
-            src={book.cover}
-            alt="cover"
-          />
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10">
+          <div class="card mb-3">
+            <div class="row no-gutters">
+              <div class="col-lg-4">
+                <img src={book.cover} class="card-img" alt="book cover" />
+              </div>
+              <div class="col-lg-8">
+                <div class="card-body">
+                  <h2 class="card-title">{book.title}</h2>
+                  <h5 id="bookauthor" class="card-title">
+                    By {book.author}
+                  </h5>
+                  <p class="card-text">{book.desc}</p>
+                  <p id="bookdetails" class="card-text">
+                    <span style={{ paddingRight: "71px", fontWeight: "500" }}>
+                      PUBLISHER
+                    </span>
+                    {book.publisher}
+                  </p>
+                  <p id="bookdetails" class="card-text">
+                    <span style={{ paddingRight: "40px", fontWeight: "500" }}>
+                      PUBLISHED ON
+                    </span>
+                    {book.date}
+                  </p>
+                  <p id="bookdetails" class="card-text">
+                    <span style={{ paddingRight: "102px", fontWeight: "500" }}>
+                      GENRE
+                    </span>
+                    {book.genre}
+                  </p>
+                  <hr />
+                  <p id="bookdetails" class="card-text">
+                    <span style={{ paddingRight: "110px", fontWeight: "500" }}>
+                      PRICE
+                    </span>
+                    {book.price}
+                  </p>
+                  <button
+                    id="submitbutton"
+                    type="submit"
+                    class="btn btn-success"
+                  >
+                    Purchase
+                  </button>
+                  <button id="detailbutton" type="submit" class="btn btn-info">
+                    About Author
+                  </button>
+                  <button id="detailbutton" type="submit" class="btn btn-dark">
+                    Back to results
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div id="bookdescription" class="col-md-8">
-          <h2>{book.title}</h2>
-          <h4>By {book.author}</h4>
-          <p style={{ paddingTop: "1vh" }}>{book.desc}</p>
-          <p>
-            <span style={{paddingRight: "40px"}}>PUBLISHER</span>
-            {book.publisher}
-          </p>
-
-          <p>
-            <span id="otherbookdetails" style={{paddingRight: "10px"}}>PUBLISHED ON</span>
-            {book.date}
-          </p>
-        </div>
+        <div class="col-lg-1"></div>
       </div>
     </div>
   );
