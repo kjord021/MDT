83d0bad62c9be8834622ae453c615bace92bb24f
Miguel Pacillo <mpaci006@fiu.edu>
2021-02-12 15:29:13 -0500

    Separated BookInfo into subcomponents

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BookInfo from "./subcomponents/BookInfo";
 
 // Placeholder book object (this will be replaced by a fetch later on)
 
@@ -20,66 +21,7 @@ const book = {
 function BookDetails() {
   return (
     <div class="container" id="detailscontainer">
-      <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10">
-          <div class="card mb-3">
-            <div class="row no-gutters">
-              <div class="col-lg-4">
-                <img src={book.cover} class="card-img" alt="book cover" />
-              </div>
-              <div class="col-lg-8">
-                <div class="card-body">
-                  <h2 class="card-title">{book.title}</h2>
-                  <h5 id="bookauthor" class="card-title">
-                    By {book.author}
-                  </h5>
-                  <p class="card-text">{book.desc}</p>
-                  <p id="bookdetails" class="card-text">
-                    <span style={{ paddingRight: "71px", fontWeight: "500" }}>
-                      PUBLISHER
-                    </span>
-                    {book.publisher}
-                  </p>
-                  <p id="bookdetails" class="card-text">
-                    <span style={{ paddingRight: "40px", fontWeight: "500" }}>
-                      PUBLISHED ON
-                    </span>
-                    {book.date}
-                  </p>
-                  <p id="bookdetails" class="card-text">
-                    <span style={{ paddingRight: "102px", fontWeight: "500" }}>
-                      GENRE
-                    </span>
-                    {book.genre}
-                  </p>
-                  <hr />
-                  <p id="bookdetails" class="card-text">
-                    <span style={{ paddingRight: "110px", fontWeight: "500" }}>
-                      PRICE
-                    </span>
-                    {book.price}
-                  </p>
-                  <button
-                    id="submitbutton"
-                    type="submit"
-                    class="btn btn-success"
-                  >
-                    Purchase
-                  </button>
-                  <button id="detailbutton" type="submit" class="btn btn-info">
-                    About Author
-                  </button>
-                  <button id="detailbutton" type="submit" class="btn btn-dark">
-                    Back to results
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-1"></div>
-      </div>
+      <BookInfo book={book} />;
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,112 +1,122 @@
-import React, {useState, useEffect} from "react";
-import {Redirect} from "react-router-dom";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
-function Login(props){
+function Login(props) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirectState, setRedirect] = useState(false);
+  const [failedLogin, setFailedLogin] = useState(false);
 
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [redirectState, setRedirect] = useState(false);
-    const [failedLogin, setFailedLogin] = useState(false);
-    
-    function onFormSubmit(e){
+  function onFormSubmit(e) {
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
 
-        const config = {
-            headers: {
-             'Content-Type': 'application/x-www-form-urlencoded'   
+    const params = {
+      userName: userName,
+      params: {
+        userName: userName,
+      },
+    };
+
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/users/login", {
+        userName: userName,
+        password: password,
+      })
+      .then(
+        (response) => {
+          console.log(response);
+          setRedirect(true);
+          props.setLoggedIn(true);
+          setFailedLogin(false);
+
+          axios.get("http://localhost:5000/users/user/", params, config).then(
+            (response) => {
+              var data = response.data;
+              props.getUserInformation(JSON.stringify(data));
+            },
+            (error) => {
+              console.log(error);
             }
+          );
+        },
+        (error) => {
+          console.log(error);
+          setRedirect(false);
+          props.setLoggedIn(false);
+          setFailedLogin(true);
         }
+      );
+  }
 
-        const params = {
-            userName: userName,
-            params: {
-                userName: userName
-            }
-        }
-
-        e.preventDefault();
-        axios.post('http://localhost:5000/users/login', {
-            userName: userName,
-            password: password
-        })
-        .then((response) => {
-            console.log(response);
-            setRedirect(true);
-            props.setLoggedIn(true);
-            setFailedLogin(false);
-
-            axios.get('http://localhost:5000/users/user/', params, config)
-            .then((response) => {
-                var data = response.data;
-                props.getUserInformation(JSON.stringify(data));
-            }
-            , (error) => {
-                console.log(error);
-            }
-            );
-
-        }, (error) => {
-            console.log(error);
-            setRedirect(false);
-            props.setLoggedIn(false);
-            setFailedLogin(true);
-        });
-
-        
-    }
-
-    
-
-    return (
-        <>
-            {redirectState ? <Redirect to="/dashboard" /> : null}
-            {failedLogin ? <div class="container">
-                                <div class = "alert alert-danger">
-                                    LOGIN FAILED
-                                </div>
-                            </div> : null}
-            <div id="background-login">
-                <div class="container">
-                    <center class="card-center">
-                        <div class="row" > 
-                            <div class="col-lg-3">
-                            </div>
-                            <div class="col-lg-3" id="middle">
-                                
-                                <div class="card bg-secondary text-white" id="loginCard">
-                                    <div class="card-header">
-                                        <p>
-                                        <h1 class="Welcome-text">Welcome Back!</h1>
-                                        Please enter your credentials to log into BookViewer-9000.
-                                        </p>
-                                    </div>
-                                    <div class="card-body">
-                                        <form onSubmit={onFormSubmit}>
-                                            <div class = "form-group">
-                                                <label for="formControlUserName">Username</label>
-                                                <input type="userName" class="form-control" id="formControlUserName" placeholder="Username" name="userName" onChange={e => setUserName(e.target.value)}/>
-                                            </div>
-                                            <div class = "form-group">
-                                                <label for="FormControlPassword">Password</label>
-                                                <input type="password" class="form-control" id="FormControlPassword" placeholder="******" name="password" onChange={e => setPassword(e.target.value)}/>
-                                            </div>
-                                            <div class = "form-group">
-                                                <center>
-                                                    <button type="submit" class="btn btn-dark">Login</button>
-                                                </center>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3">
-                            </div>                      
-                        </div>
-                    </center>
+  return (
+    <>
+      {redirectState ? <Redirect to="/dashboard" /> : null}
+      {failedLogin ? (
+        <div class="container">
+          <div class="alert alert-danger">LOGIN FAILED</div>
+        </div>
+      ) : null}
+      <div id="background-login">
+        <div class="container">
+          <center class="card-center">
+            <div class="row">
+              <div class="col-lg-3"></div>
+              <div class="col-lg-3" id="middle">
+                <div class="card bg-secondary text-white" id="cardLogin">
+                  <div class="card-header">
+                    <p>
+                      <h1 class="Welcome-text">Welcome Back!</h1>
+                      Please enter your credentials to log into BookViewer-9000.
+                    </p>
+                  </div>
+                  <div class="card-body">
+                    <form onSubmit={onFormSubmit}>
+                      <div class="form-group">
+                        <label for="formControlUserName">Username</label>
+                        <input
+                          type="userName"
+                          class="form-control"
+                          id="formControlUserName"
+                          placeholder="Username"
+                          name="userName"
+                          onChange={(e) => setUserName(e.target.value)}
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="FormControlPassword">Password</label>
+                        <input
+                          type="password"
+                          class="form-control"
+                          id="FormControlPassword"
+                          placeholder="******"
+                          name="password"
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </div>
+                      <div class="form-group">
+                        <center>
+                          <button type="submit" class="btn btn-dark">
+                            Login
+                          </button>
+                        </center>
+                      </div>
+                    </form>
+                  </div>
                 </div>
+              </div>
+              <div class="col-lg-3"></div>
             </div>
-        </>
-    );
+          </center>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
