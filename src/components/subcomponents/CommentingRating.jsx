ed1f1c156f5ff480133a0a8371c554bd90caf23c
Christopher Perez <chrisperez599@gmail.com>
2021-02-26 17:10:20 -0500

    Star rating System added. Create review now seperate, loads on click.

--- src/components/subcomponents/CommentingRating.jsx
+++ src/components/subcomponents/CommentingRating.jsx
@@ -1,69 +1,129 @@
 import React, { useState } from "react";
-const mongoose = require("mongoose");
+import StarRatings from "react-star-ratings";
 
 function CommentingRating(props) {
+  const [createReview, setCreateReview] = useState(false);
   const [headline, setHeadline] = useState("");
   const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(0);
 
-  return (
-    <div class="row" align="center">
-      <div class="col-lg-1"></div>
-      <div class="col-lg-10 ">
-        <div class="card mb-3">
-          <div class="row no-gutters">
-            <br />
-            <div class="col-lg-5">
+  if (createReview) {
+    return (
+      <div class="row" align="center">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 ">
+          <div class="card mb-3">
+            <div class="row no-gutters">
               <br />
-              <h3>Create Review</h3>
-            </div>
-            <div class="col-lg-12">
-              <h4>Overall Rating</h4>
-              <div class="rating">
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
+              <div class="col-lg-5">
+                <br />
+                <h3>Create Review</h3>
               </div>
-              <br />
-              <hr />
-            </div>
-            <div class="col-lg-12">
-              <form>
+              <div class="col-lg-12">
+                <h4>Overall Rating</h4>
+                <div class="rating">
+                  <StarRatings
+                    rating={rating}
+                    changeRating={(e) => setRating(e)}
+                    starRatedColor="blue"
+                    numberOfStars={5}
+                    name="creat rating"
+                    isSelectable
+                    starDimension="35px"
+                  />
+                </div>
                 <br />
-                <h4>Add a Headline</h4>
-                <br />
-                <input
-                  size="97"
-                  placeholder="What's most important to know?"
-                  onChange={(e) => setHeadline(e.target.value)}
-                />
-                <br />
-                <br />
-                <br />
-                <h4>Add a Comment</h4>
-                <br />
-                <textarea
-                  placeholder="What did you like or dislike?"
-                  name="review_text"
-                  cols="100"
-                  rows="5"
-                  onChange={(e) => setComment(e.target.value)}
-                ></textarea>
-                <br />
-                <br />
-                <button type="submit" class="btn btn-dark">
-                  Submit Review
-                </button>
-                <br />
-                <br />
-              </form>
+                <hr />
+              </div>
+              <div class="col-lg-12">
+                <form>
+                  <br />
+                  <h4>Add a Headline</h4>
+                  <br />
+                  <input
+                    size="97"
+                    placeholder="What's most important to know?"
+                    onChange={(e) => setHeadline(e.target.value)}
+                  />
+                  <br />
+                  <br />
+                  <br />
+                  <h4>Add a Comment</h4>
+                  <br />
+                  <textarea
+                    placeholder="What did you like or dislike?"
+                    name="review_text"
+                    cols="100"
+                    rows="5"
+                    onChange={(e) => setComment(e.target.value)}
+                  ></textarea>
+                  <br />
+                  <br />
+                  <button type="submit" class="btn btn-dark">
+                    Submit Review
+                  </button>
+                  <br />
+                  <br />
+                </form>
+              </div>
             </div>
           </div>
         </div>
+        <div class="col-lg-1"></div>
       </div>
-      <div class="col-lg-1"></div>
-    </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div class="row" align="center">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 ">
+          <div class="card mb-3">
+            <div class="row no-gutters"></div>
+            <br></br>
+            <h2>Customer Reviews</h2>
+            <br />
+            <div class="row">
+              <div class="col-lg-2"></div>
+              <div class="col-lg-4" align="center">
+                <h3>Overall Rating</h3>
+                <StarRatings
+                  rating={4.4}
+                  starRatedColor="blue"
+                  numberOfStars={5}
+                  name="rating"
+                  starDimension="35px"
+                />
+                <h4> {4.4} out of 5</h4>
+              </div>
+
+              <div class="col-lg-4" align="Center">
+                <br />
+                <br />
+                <button
+                  onClick={() => setCreateReview(true)}
+                  class="btn btn-outline-secondary btn-lg"
+                >
+                  Create Customer Review
+                </button>
+              </div>
+              <div class="col-lg-2"></div>
+            </div>
+            <br></br>
+            <br></br>
+            <h3>Featured Reviews</h3>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        </div>
+        <div class="col-lg-1"></div>
+      </div>
+    </React.Fragment>
   );
 }
 
